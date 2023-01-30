595b03af2997366471af45c9ea8fad04a423bef8
autumn-caer <muestra114@gmail.com>
2023-01-31 07:50:07 +0900

    routing example

--- front/pages/posts/first-post.tsx
+++ front/pages/posts/first-post.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <Head>
+        <title>first-post</title>
+      </Head>
+      <h1>First Post aa</h1>
+      <Link href="/">Back to Home</Link>
+    </>
+  );
 }
---
+++ front/pages/posts/second-post.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function SecondPost() {
+  return (
+    <>
+      <h1>Second Post aaaa</h1>
+      <Link href="/">Back to Home</Link>
+    </>
+  );
+}
